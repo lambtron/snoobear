77b658defb122861503c3b89d7dc963a6b3b288b
Andy Jiang <andyjiang@gmail.com>
2015-11-01 17:23:57 -0800

    updated client/js/worker.js

--- client/js/worker.js
+++ client/js/worker.js
@@ -6,4 +6,5 @@
 self.addEventListener('message', function(e) {
   self.postMessage('submit');
   setInterval(function() { self.postMessage('submit') }, 3960000);
+  // setInterval(function() { self.postMessage('submit') }, 5000);
 });
